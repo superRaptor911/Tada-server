3462b964af9de08ed1b03e3fa184cafc89f948d5
superRaptor911 <raptor.inc2018@gmail.com>
2025-06-01 00:44:47 +0530

    initial commit

---
+++ gpt.py
@@ -0,0 +1,30 @@
+import openai
+from typing import List, Dict, Any
+
+MessageType = Dict[str, str] 
+
+def get_ai_chat_response(messages: List[MessageType], model="gpt-4.1-mini-2025-04-14"):
+    try:
+        response = openai.chat.completions.create(
+            model=model,
+            messages=messages,
+        )
+        return response.choices[0].message.content  # Extract response text
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+    
+
+def gpt_simplify_text(text: str) -> str:
+    messages = [
+        {"role": "system", "content": "You will be given innerText of a webpage. Your task is to simplify the text, making it easier to understand. You should not change the meaning of the text, but rather make it more accessible."},
+        {"role": "user", "content": text}
+    ]
+    
+    response = get_ai_chat_response(messages)
+    print("got response from gpt")
+    if response:
+        return response
+    else:
+        return "Error in processing the request."
---
+++ main.py
@@ -0,0 +1,56 @@
+from dotenv import load_dotenv
+from gpt import gpt_simplify_text  # Import the function from gpt.py
+from concurrent.futures import ThreadPoolExecutor
+# Load environment variables from .env
+load_dotenv()
+
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Configure CORS
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,             # Allowed origins
+    allow_credentials=True,            # Allow cookies and authentication
+    allow_methods=["*"],               # Allow all HTTP methods
+    allow_headers=["*"],               # Allow all headers
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+
+
+class Content(BaseModel):
+    content: str
+
+@app.post("/simplify")
+def simplify_text(payload: Content):
+    chunk_Size = 8000
+    print(f"Received content of length: {len(payload.content)}")
+    chunks = [payload.content[i:i + chunk_Size] for i in range(0, len(payload.content), chunk_Size)]
+    responses = []
+    with ThreadPoolExecutor() as executor:
+        responses = list(executor.map(gpt_simplify_text, chunks))
+
+    print(f"Processed {len(chunks)} chunks.")
+
+    return {"content": "\n".join(responses)}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "main:app",  # assume the file is named main.py
+        host="0.0.0.0",  # listen on all available network interfaces
+        port=8000,  # default port
+        reload=True  # enable auto-reload for development
+    )
